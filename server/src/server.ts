009ae86db8fed63078480df4ec30bfc05477fd8b
Adam <latifasedyeame@gmail.com>
2024-06-09 22:21:31 -0600

    controller/routes/model

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-dotenv.config();
+import connectDB from "./config/databaseConfig";
+import shortUrl from "./routes/shortUrl";
 
+dotenv.config();
+connectDB();
 // PORT
 const port = process.env.PORT || 5001;
 // create express app
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors({ origin: "http://localhost:3000", credentials: true}));
 
-app.get("/", (req, res) => {
-    res.send("Hellow");
-});
+app.use("/api/", shortUrl);
 
 app.listen(port, () => {
     console.log(`Server started successfuly on PORT: ${port}`);
