172bcb7da60bfc057f5f8188bd16511b51e3f56c
Adam <latifasedyeame@gmail.com>
2024-06-10 23:32:17 -0600

    update

--- server/src/controllers/shortUrl.ts
+++ server/src/controllers/shortUrl.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { urlModel } from "../model/shortUrl";
-import { url } from "inspector";
 
 export const createUrl = async (req: express.Request, res: express.Response) => {
     try {
@@ -34,8 +33,29 @@ export const getAllUrl = async (req: express.Request, res: express.Response) => 
     }
 }
 export const getUrl = async (req: express.Request, res: express.Response) => {
-    
+    try{
+        const shortUrl = await urlModel.findOne({shortUrl: req.params.id});
+        if(!shortUrl) {
+            res.status(404).send({message: "Full URL not found."});
+        }
+        else {
+            shortUrl.clicks++;
+            shortUrl.save();
+            res.redirect(`${shortUrl.fullUrl}`);
+        }
+    }
+    catch(erorr) {
+        res.status(500).send({message: "Something went wrong."});
+    }
 }
 export const deleteUrl = async (req: express.Request, res: express.Response) => {
-    
+    try{
+        const shortUrl = await urlModel.findByIdAndDelete({_id: req.params.id});
+        if(shortUrl) {
+            res.status(200).send({message: "Requested URL deleted."});
+        }
+    }
+    catch(erorr) {
+        res.status(500).send({message: "Something went wrong."});
+    }
 }
